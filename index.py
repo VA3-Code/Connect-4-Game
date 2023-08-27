e025fe2b9d2a7c3dee272d2bd1fab826d8100ff2
VA3-Code <79648277+VA3-Code@users.noreply.github.com>
2023-08-27 18:31:13 +0530

    Added icon and caption

--- index.py
+++ index.py
@@ -7,6 +7,10 @@ pygame.init()
 # Create screen
 screen = pygame.display.set_mode((960, 540), pygame.RESIZABLE)
 
+pygame.display.set_caption("Connect 4 - Game developed by Mohit and Vinayak")
+
+pygame.display.set_icon(pygame.display.load_image("assets/icon.png"))
+
 # Game Loop
 running = True
 
